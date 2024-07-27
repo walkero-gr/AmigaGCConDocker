6297a169f2f6eb019a32e1d7874094aea24e6bfd
George Sokianos <walkero@gmail.com>
2024-07-27 21:15:57 +0100

    More changes

--- ppc-amigaos/scripts/libs/clib4_libs.sh
+++ ppc-amigaos/scripts/libs/clib4_libs.sh
@@ -77,9 +77,10 @@ echo -e "${CCPINK}${CCBOLD}\n---> Install clib4 libraries${CCEND}";
 	curl -fsSL https://clib2pkg.amigasoft.net/ubuntu/clib4.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/clib4.gpg && \
 		echo "deb [arch=amd64] https://clib2pkg.amigasoft.net/ubuntu/ focal main" | tee /etc/apt/sources.list.d/clib4.list && \
 		rm -rf /var/lib/apt/lists/* && \
-		apt-get clean && apt-get update --fix-missing;
+		apt-get clean && \
+		apt-get update -o Acquire::CompressionTypes::Order::=gz;
 	# This is a workaround to fix the issue with the "Hash Sum mismatch" error
-	until apt-get update --fix-missing; do echo trying again; done
+
 	apt-get -y --no-install-recommends -o Dpkg::Options::="--force-overwrite" install $CLIB4_PACKAGES
 	\cp -r /usr/ppc-amigaos/SDK/* ${SDK_PATH}/
 
