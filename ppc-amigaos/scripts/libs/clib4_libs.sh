ee42de12a2b45810243de47de6654f2bbf1fbd47
George Sokianos <walkero@gmail.com>
2024-07-27 19:47:35 +0100

    Trying to clean the cached apt lists before installing clib4 libs

--- ppc-amigaos/scripts/libs/clib4_libs.sh
+++ ppc-amigaos/scripts/libs/clib4_libs.sh
@@ -74,6 +74,7 @@ echo -e "${CCPINK}${CCBOLD}\n---> Install clib4 libraries${CCEND}";
 	# Install clib4 libraries from afxgroup's Ubuntu repo.
 	# They are saved under /user/ppc-amigaos
 	dpkg --add-architecture amd64;
+	rm -rf /var/lib/apt/lists/*;
 	curl -fsSL https://clib2pkg.amigasoft.net/ubuntu/clib4.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/clib4.gpg && \
 		echo "deb [arch=amd64] https://clib2pkg.amigasoft.net/ubuntu/ focal main" | tee /etc/apt/sources.list.d/clib4.list && \
 		apt-get update;
