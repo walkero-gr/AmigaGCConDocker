8313ecabad4f8569a963b605808eb1d449ee5057
George Sokianos <walkero@gmail.com>
2024-11-25 18:40:54 +0000

    MUI SDK is now integrated with the SDK

--- ppc-amigaos/scripts/libs/mui5.sh
+++ ppc-amigaos/scripts/libs/mui5.sh
@@ -6,5 +6,9 @@ echo -e "${CCPINK}${CCBOLD}\n---> Install MUI 5.0 SDK${CCEND}";
 	lha -xfq2 MUI-5.0.lha; \
 	curl -fsSL "https://github.com/amiga-mui/muidev/releases/download/MUI-5.0-20210831/MUI-5.0-20210831-os4-contrib.lha" -o /tmp/MUI-5.0-contrib.lha && \
 	lha -xfq2 MUI-5.0-contrib.lha; \
-	mv ./SDK/MUI ${SDK_PATH}/MUI_5.0; \
+	mkdir -p ${SDK_PATH}/Examples/MUI; \
+	mv ./SDK/MUI/Autodocs/* ${SDK_PATH}/Documentation/AutoDocs/; \
+	mv ./SDK/MUI/C/Examples/* ${SDK_PATH}/Examples/MUI/; \
+	\cp -r ./SDK/MUI/C/include/* ${SDK_PATH}/local/common/include/; \
+	mv ./SDK/MUI/Docs ${SDK_PATH}/local/Documentation/MUI; \
 	rm -rf /tmp/*;
