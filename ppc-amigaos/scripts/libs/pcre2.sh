9b1a5021a384c94662322687df73301fdff748f3
George Sokianos <walkero@gmail.com>
2024-12-17 08:51:15 +0000

    Updated pcre2 to version 10.44

--- ppc-amigaos/scripts/libs/pcre2.sh
+++ ppc-amigaos/scripts/libs/pcre2.sh
@@ -2,7 +2,7 @@
 # 
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install pcre2${CCEND}";
-	curl -fsSL "https://github.com/AmigaLabs/libs-ports/raw/main/pcre2/pcre2-10.42.lha" -o /tmp/pcre2.lha && \
+	curl -fsSL "https://github.com/AmigaLabs/libs-ports/raw/main/pcre2/pcre2-10.44.lha" -o /tmp/pcre2.lha && \
 		lha -xfq2 pcre2.lha && \
 		cp -r ./SDK/local/clib2/* ${SDK_PATH}/local/clib2/ && \
 		cp -r ./SDK/local/newlib/* ${SDK_PATH}/local/newlib/ && \
