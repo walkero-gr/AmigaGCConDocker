667aa0ee58f9fbae9a3a7ac1408bca535322fd73
George Sokianos <walkero@gmail.com>
2024-12-17 08:50:48 +0000

    Updated liblua to 5.4.7

--- ppc-amigaos/scripts/libs/liblua.sh
+++ ppc-amigaos/scripts/libs/liblua.sh
@@ -2,7 +2,7 @@
 # 
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install liblua 5.4${CCEND}";
-	curl -fsSL "https://github.com/AmigaLabs/libs-ports/raw/main/lua5.4/lua-5.4.6.lha" -o /tmp/liblua.lha && \
+	curl -fsSL "https://github.com/AmigaLabs/libs-ports/raw/main/lua5.4/lua-5.4.7.lha" -o /tmp/liblua.lha && \
 		lha -xfq2 liblua.lha && \
 		cp -r ./SDK/local/* ${SDK_PATH}/local/ && \
 		rm -rf /tmp/*;
