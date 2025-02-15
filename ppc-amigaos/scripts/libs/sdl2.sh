f6d8a62d7e8586eac1b180d536da5eab0af3e3df
George Sokianos <walkero@gmail.com>
2025-02-15 18:28:26 +0000

    Updated SDL2 to 2.30.9

--- ppc-amigaos/scripts/libs/sdl2.sh
+++ ppc-amigaos/scripts/libs/sdl2.sh
@@ -2,7 +2,7 @@
 # 
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install SDL2 SDK${CCEND}";
-	curl -fsSL "https://github.com/AmigaPorts/SDL/releases/download/v2.30.4-amigaos4/SDL2.lha" -o /tmp/SDL2.lha && \
+	curl -fsSL "https://github.com/AmigaPorts/SDL/releases/download/v2.30.9-amigaos4/SDL2.lha" -o /tmp/SDL2.lha && \
 		lha -xfq2 SDL2.lha && \
 		cp -r ./SDL2/SDK/local/* ${SDK_PATH}/local/ && \
 		mkdir ${SDK_PATH}/local/Documentation/SDL2 && \
@@ -10,4 +10,6 @@ echo -e "${CCPINK}${CCBOLD}\n---> Install SDL2 SDK${CCEND}";
 		mv ./SDL2/README-SDL.txt ${SDK_PATH}/local/Documentation/SDL2/ && \
 		mv ./SDL2/README-amigaos4.md ${SDK_PATH}/local/Documentation/SDL2/ && \
 		mv ./SDL2/WhatsNew.txt ${SDK_PATH}/local/Documentation/SDL2/ && \
+		chmod +x ${SDK_PATH}/local/newlib/bin/sdl2-config && \
+		sed -i 's/^prefix=.*/prefix=${SDK_PATH}\/local\/newlib/' ${SDK_PATH}/local/newlib/bin/sdl*config && \
 		rm -rf /tmp/*;
