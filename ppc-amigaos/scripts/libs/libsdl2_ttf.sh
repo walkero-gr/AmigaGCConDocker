7072fbd6110eb9608bff02a7bfd97c1710e8be0b
George Sokianos <walkero@gmail.com>
2025-02-15 18:29:27 +0000

    Added libsdl2_ttf 2.22.0 for newlib

---
+++ ppc-amigaos/scripts/libs/libsdl2_ttf.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# 
+
+echo -e "${CCPINK}${CCBOLD}\n---> Install libsdl2_ttf${CCEND}";
+	curl -fsSL "https://os4depot.net/share/development/library/graphics/libsdl2_ttf.lha" -o /tmp/libsdl2_ttf.lha && \
+		lha -xfq2 libsdl2_ttf.lha && \
+		cp -r ./SDL2_ttf-2.22.0/SDK/local/* ${SDK_PATH}/local/ && \
+		rm -rf /tmp/*;
