8e1bddf86af73f8ba0895a3adb32297bfc810bc8
George Sokianos <walkero@gmail.com>
2026-02-04 19:58:34 +0000

    Updated SDL3 to 3.4.0

--- ppc-amigaos/scripts/libs/sdl3.sh
+++ ppc-amigaos/scripts/libs/sdl3.sh
@@ -2,7 +2,7 @@
 # 
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install SDL3 SDK${CCEND}";
-	curl -fsSL "https://github.com/AmigaPorts/SDL/releases/download/v3.2.20-amigaos4/SDL3.lha" -o /tmp/SDL3.lha && \
+	curl -fsSL "https://github.com/AmigaPorts/SDL/releases/download/v3.4.0-amigaos4/SDL3.lha" -o /tmp/SDL3.lha && \
 		lha -xfq2 SDL3.lha && \
 		cp -r ./SDL3/SDK/local/* ${SDK_PATH}/local/ && \
 		mkdir -p ${SDK_PATH}/local/Documentation/SDL3 && \
