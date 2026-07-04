89a6ab5ac73fb5f9c4aa6d25dd8885ff13fd97f9
George Sokianos <walkero@gmail.com>
2026-07-04 10:50:48 +0100

    Updated amissl to v5.27

--- ppc-amigaos/scripts/libs/amissl5.sh
+++ ppc-amigaos/scripts/libs/amissl5.sh
@@ -2,7 +2,7 @@
 # 
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install AmiSSL SDK${CCEND}";
-	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://github.com/jens-maus/amissl/releases/download/5.26/AmiSSL-5.26-SDK.lha" -o /tmp/AmiSSL.lha && \
+	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://github.com/jens-maus/amissl/releases/download/5.27/AmiSSL-5.27-SDK.lha" -o /tmp/AmiSSL.lha && \
 		lha -xfq2 AmiSSL.lha && \
 		cp -r ./AmiSSL/Developer/include/* ${SDK_PATH}/include/include_h/ && \
 		cp -r ./AmiSSL/Developer/xml/* ${SDK_PATH}/include/interfaces/ && \
