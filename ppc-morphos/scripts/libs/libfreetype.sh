d2882338a1aa2374b1d299af4cb9a16ba71d6745
George Sokianos <walkero@gmail.com>
2026-09-05 20:43:49 +0100

    Updated the freetype library in the ppc-morphos image

--- ppc-morphos/scripts/libs/libfreetype.sh
+++ ppc-morphos/scripts/libs/libfreetype.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
-# 
+#
 set -e
 
 echo -e "${CCPINK}${CCBOLD}\n---> Install libfreetype ${CCEND}";
-	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://www.morphos-storage.net/dl.php?id=2299476" -o libfreetype.lha && \
-		lha -xfq2 libfreetype.lha && \
-		mkdir -p /gg/usr/local/lib && \
-		cp -r ./freetype-2.13.3/builds/amiga/libfreetype.a /gg/usr/local/lib/libfreetype.a && \
-		cp -r ./freetype-2.13.3/include /gg/usr/local/ && \
+	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://github.com/walkero-gr/morphos-libs/raw/refs/heads/main/freetype/freetype-2.14.3.lha" -o /tmp/freetype.lha && \
+		lha -xfq2 freetype.lha && \
+		cp -r ./freetype-2.14.3/SDK/* /gg/ && \
 		rm -rf /tmp/*;
