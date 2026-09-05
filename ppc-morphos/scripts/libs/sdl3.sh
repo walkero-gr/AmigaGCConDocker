10f1ee082f3f9e48bb666af5226c4aaa009c20dc
George Sokianos <walkero@gmail.com>
2026-09-05 20:42:52 +0100

    Update SDL3 in the ppc-morphos

--- ppc-morphos/scripts/libs/sdl3.sh
+++ ppc-morphos/scripts/libs/sdl3.sh
@@ -2,8 +2,15 @@
 #
 set -e
 
+# echo -e "${CCPINK}${CCBOLD}\n---> Install SDL3 SDK ${CCEND}";
+# 	curl -fsSL "https://www.morphos-storage.net/dl.php?id=2403006" -o /tmp/SDL3.lha && \
+# 		lha -xfq2 SDL3.lha && \
+# 		cp -r ./SDL3-3.2.18/SDK/* /gg/ && \
+# 		rm -rf /tmp/*;
+
+
 echo -e "${CCPINK}${CCBOLD}\n---> Install SDL3 SDK ${CCEND}";
-	curl -fsSL "https://www.morphos-storage.net/dl.php?id=2403006" -o /tmp/SDL3.lha && \
+	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://www.morphos-storage.net/dl.php?id=2467903" -o /tmp/SDL3.lha && \
 		lha -xfq2 SDL3.lha && \
-		cp -r ./SDL3-3.2.18/SDK/* /gg/ && \
+		cp -r ./SDL3/SDK/* /gg/ && \
 		rm -rf /tmp/*;
