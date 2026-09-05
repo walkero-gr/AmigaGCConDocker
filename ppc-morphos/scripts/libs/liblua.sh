1c86dfed12af1d7f049e73419dee714e1b660370
George Sokianos <walkero@gmail.com>
2026-09-05 20:43:28 +0100

    Updated the lua and pcre2 libraries in the ppc-morphos image

--- ppc-morphos/scripts/libs/liblua.sh
+++ ppc-morphos/scripts/libs/liblua.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-# 
+#
 set -e
 
-echo -e "${CCPINK}${CCBOLD}\n---> Install liblua 5.4${CCEND}";
-	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://github.com/walkero-gr/morphos-libs/raw/refs/heads/main/lua5.4/lua-5.4.7.lha" -o /tmp/lua.lha && \
-		lha -xfq2 lua.lha && \
-		cp -r ./SDK/* /gg/ && \
+echo -e "${CCPINK}${CCBOLD}\n---> Install liblua 5.4 ${CCEND}";
+	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://github.com/walkero-gr/morphos-libs/raw/refs/heads/main/lua5.4/lua-5.4.9.lha" -o /tmp/lua54.lha && \
+		lha -xfq2 lua54.lha && \
+		cp -r ./lua-5.4.9/SDK/* /gg/ && \
 		rm -rf /tmp/*;
