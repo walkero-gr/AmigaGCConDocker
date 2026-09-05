feaef436e18ab6cbdbd1a1c3e4bbd8eec3bf0524
George Sokianos <walkero@gmail.com>
2026-09-05 20:42:19 +0100

    Disabled the tinygl and the sqlite packages for ppc-morphos

--- ppc-morphos/scripts/libs/tinygl.sh
+++ ppc-morphos/scripts/libs/tinygl.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-# 
+#
 set -e
 
-echo -e "${CCPINK}${CCBOLD}\n---> Install TinyGL SDK${CCEND}";
-	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://tinygl.molsen.co.za/TinyGL-Update-2024-09-29.lha" -o /tmp/tinygl.lha && \
-		lha -xfq2 tinygl.lha && \
-		cp -r ./TinyGL-Update-2024-09-29/SDK/GG/* /gg/ && \
-		rm -rf /tmp/*;
+# echo -e "${CCPINK}${CCBOLD}\n---> Install TinyGL SDK${CCEND}";
+# 	curl --retry 5 --retry-delay 2 --retry-connrefused -fsSL "https://tinygl.molsen.co.za/TinyGL-Update-2024-09-29.lha" -o /tmp/tinygl.lha && \
+# 		lha -xfq2 tinygl.lha && \
+# 		cp -r ./TinyGL-Update-2024-09-29/SDK/GG/* /gg/ && \
+# 		rm -rf /tmp/*;
